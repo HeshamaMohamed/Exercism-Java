39de03a23acf242a85cc3ab62926113fb2dbf53a
Hesham <heshamamohamed323@gmail.com>
2023-12-03 11:35:45 +0200

    Add Interfaces: Remote Control Competition Solution

---
+++ Interfaces/remote-control-competition/src/main/java/ProductionRemoteControlCar.java
@@ -0,0 +1,24 @@
+class ProductionRemoteControlCar implements RemoteControlCar, Comparable<ProductionRemoteControlCar> {
+    private int distanceTravelled = 0;
+    private int numberOfVictories = 0;
+
+    @Override
+    public int compareTo(ProductionRemoteControlCar car){
+        return Integer.compare(car.getNumberOfVictories(), getNumberOfVictories());
+    }
+    public void drive() {
+        this.distanceTravelled += 10;
+    }
+
+    public int getDistanceTravelled() {
+        return this.distanceTravelled;
+    }
+
+    public int getNumberOfVictories() {
+        return this.numberOfVictories;
+    }
+
+    public void setNumberOfVictories(int numberOfVictories) {
+        this.numberOfVictories = numberOfVictories;
+    }
+}
